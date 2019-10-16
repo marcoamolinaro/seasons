78a8145c4c69dfd213767244f1b254750b08183c
marcoamolinaro <molinaromarcoaurelio@gmail.com>
2019-10-16 18:05:49 -0300

    51. Refac from Functional to Class Components

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
-const App = () => {
-    window.navigator.geolocation.getCurrentPosition(
-        position => console.log(position),
-        err => console.log(err)
-    );
-    return <div>Hello World!</div>
-};
+class App extends React.Component {
+
+    render () {
+        window.navigator.geolocation.getCurrentPosition(
+            position => console.log(position),
+            err => console.log(err)
+        );
+
+        return <div>Latitude: </div>;
+    }
+}
 
 ReactDom.render(<App />, document.querySelector('#root'));
